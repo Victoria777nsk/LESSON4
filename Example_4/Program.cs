4def1392d55eea63d15fee254a1dccef00bb9798
Victoria <victoriaerygina@yandex.ru>
2022-09-04 12:59:34 +0700

    Рекурсия (Последоват-ть Фибоначчи

---
+++ Example_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Последовательность Фибоначчи:
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Fibonacci (int n)
+{
+    if (n == 1 || n == 2) return 1; // "||" = "или"
+    else return Fibonacci (n - 1) + Fibonacci (n - 2);
+}
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
